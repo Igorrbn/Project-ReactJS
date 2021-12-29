93a2887f5b171d441f98c6922403ba422c7b89e9
Igor <igorr.snoopy@hotmail.com>
2021-12-29 18:22:21 -0300

    Att Repo
    Att Repo

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
 
 
 const repository = {
@@ -12,15 +13,25 @@ const repository = {
 }
 
 export function RepositoryList () {
+
+ const [repositories, setRepositories] = useState([]);
+
+ useEffect(() => {
+   fetch('https://api.github.com/orgs/rocketseat/repos')
+   .then(response => response.json()) 
+   .then(data => setRepositories(data)) 
+ }, []);
+
   return (
       <section className="repository-list">
           <h1>Lista de Repositórios</h1>
 
           <ul>
-            <RepositoryItem repository={repository} />       
-            <RepositoryItem repository={repository} />
-            <RepositoryItem repository={repository} />
-            <RepositoryItem repository={repository} />
+            {repositories.map(repository => {
+              return <RepositoryItem key={repository.name} repository={repository} />  
+
+            })}            
+            
           </ul>
       </section>    
   );
